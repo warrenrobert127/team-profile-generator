05ba243866ae36e6fa99d9009cfdaceed261d087
Robert Warren <warrenrobert127@gmail.com>
2022-02-21 16:53:05 -0600

    work on tests

---
+++ __test__/Manager.test.js
@@ -0,0 +1,20 @@
+//Importing Employee constructor
+const Employee = require("./Employee");
+
+//Manager constructor extends employee constructor
+class Manager extends Employee {
+  constructor(name, id, email, officeNumber) {
+    //Calling employee constructor
+    super(name, id, email);
+
+    this.officeNumber = officeNumber;
+  }
+
+  //Override employee role to manager
+  getRole() {
+    return "Manager";
+  }
+}
+
+//To be exported
+module.exports = Manager;
